9766f286cb4438d17c4dd591c5b18754692503ec
Gleb Shalimov <sbgleb10@gmail.com>
2023-11-06 15:18:27 +0300

    Created flutter counter v 1.0 project

---
+++ flutter-task-1/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SurfApp());
+}
+
+class SurfApp extends StatelessWidget {
+  const SurfApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePageStateful(title: 'Stateful page'),
+    );
+  }
+}
+
+class HomePageStateful extends StatefulWidget {
+  final String title;
+
+  const HomePageStateful({required this.title, Key? key}) : super(key: key);
+
+  @override
+  State<HomePageStateful> createState() => _HomePageStatefulState();
+}
+
+class _HomePageStatefulState extends State<HomePageStateful> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    print('new value: $_counter');
+    setState(() {
+      _counter += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build() method called');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
