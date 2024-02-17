7e0374787c50ece698d2908c3fa1cbf9e4ec5505
Gleb Shalimov <sbgleb10@gmail.com>
2024-02-17 19:15:35 +0300

    Added comments that were deleted earlier

--- flutter-task-4/lib/models/products_model.dart
+++ flutter-task-4/lib/models/products_model.dart
@@ -21,22 +21,26 @@ class ProductEntity {
   });
 }
 
+/// Класс, описывающий количество товара.
 sealed class Amount {
   int get value;
 }
 
+/// Класс, описывающий количество товара в граммах.
 class Grams implements Amount {
   @override
   final int value;
   Grams(this.value);
 }
 
+/// Класс, описывающий количество товара в штуках.
 class Quantity implements Amount {
   @override
   final int value;
   Quantity(this.value);
 }
 
+/// Категория товара.
 enum Category {
   food('Продукты питания'),
   care('Уход');
