b57eaf8df377e47ce337aeeb4c4b961db528fd92
Gleb Shalimov <sbgleb10@gmail.com>
2024-05-27 22:41:50 +0300

    add file with Utils class

---
+++ flutter/magic_ball/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:magic_ball/screens/magic_ball.dart';
+
+void main() => runApp(const MaterialApp(
+      home: MagicBallScreen(),
+      debugShowCheckedModeBanner: false,
+    ));
