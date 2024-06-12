88f2821500d7cfde3f8cfb6592154e540ecb4f6e
Gleb Shalimov <sbgleb10@gmail.com>
2024-06-12 18:30:24 +0300

    update loading and displaying photos from storage

--- intensive/flutter-task-7/lib/data/photo_service.dart
+++ intensive/flutter-task-7/lib/data/photo_service.dart
@@ -1,16 +1,16 @@
 import 'package:surf_flutter_courses_template/api/supabase_auth.dart';
-
 import '../uikit/constants.dart';
+import 'package:supabase/supabase.dart';
 
 class PhotoService {
   Future<List<String>> fetchPhotos() async {
-    final response = await Utils.supabaseClient
-        .from(DbConstants.photoTable)
-        .select(DbConstants.imagePath);
+    final response = await Utils.supabaseClient.storage.from('photos').list();
 
     List<String> photoUrls = [];
-    for (final row in response) {
-      photoUrls.add(row[DbConstants.imagePath] as String);
+    for (final file in response) {
+      final url =
+          Utils.supabaseClient.storage.from('photos').getPublicUrl(file.name);
+      photoUrls.add(url);
     }
     return photoUrls;
   }
