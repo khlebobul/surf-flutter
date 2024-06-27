37f6c61c09f1053eaf8ed86ff44c3fa70234deaa
khlebobul <sbgleb10@gmail.com>
2024-06-27 12:43:59 +0300

    complete intensive task 9

---
+++ intensive/flutter-task-9/lib/constants.dart
@@ -0,0 +1,28 @@
+const String dogSvgPath = 'assets/dog.svg';
+const String catSvgPath = 'assets/cat.svg';
+const String parrotSvgPath = 'assets/parrot.svg';
+const String hamsterSvgPath = 'assets/hamster.svg';
+
+const String dogLabel = 'Собака';
+const String catLabel = 'Кошка';
+const String parrotLabel = 'Попугай';
+const String hamsterLabel = 'Хомяк';
+
+const String nameHintText = 'Имя питомца';
+const String birthdayHintText = 'День рождения питомца';
+const String weightHintText = 'Вес, кг';
+const String ownerEmailHintText = 'Почта хозяина';
+
+const String nameErrorText = 'Укажите имя питомца от 3 до 20 символов';
+const String birhdayErrorText = 'Укажите дату дд/мм/гггг';
+const String weightErrorText = 'Укажите вес, больше 0 кг';
+const String ownerEmailErrorText = 'Укажите почту хозяина';
+
+const String vaccinationHeadingText = 'Сделаны прививки от:';
+const String vaccinationOneText = 'бешенства';
+const String vaccinationTwoText = 'ковида';
+const String vaccinationThreeText = 'малярии';
+
+const String lastVaccinationDateText = 'Дата последней прививки';
+
+const String saveButtonText = 'Отправить';
---
+++ intensive/flutter-task-9/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:surf_flutter_courses_template/screen/home_screen.dart';
+
+void main() {
+  runApp(const FormValidationApp());
+}
+
+class FormValidationApp extends StatelessWidget {
+  const FormValidationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
